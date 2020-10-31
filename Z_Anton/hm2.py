2c9608e0066deb95d2966e9838ef7f908795b9de
tony <zakvackijanton@gmail.com>
2020-10-31 17:38:49 +0200

    s_commit

--- Z_Anton/hm2.py
+++ Z_Anton/hm2.py
@@ -1,4 +1,5 @@
 import random
+import copy
 sample_dict = {'class-_a':{
                 'student':{
                     'name':'Misha',
@@ -14,7 +15,7 @@ print(sample_dict['class-_a']['student']['marks']['history'])
 sample_dict['class-_a']['new_student'] = {'name': 'Vova', 'marks':{
     'math':100, 'history':95
 }}
-sample_dict['class_b'] = sample_dict['class-_a'].copy()
+sample_dict['class_b'] = copy.deepcopy(sample_dict['class-_a'])
 line = 'Igor', 'Anton'
 v = []
 v_a = []
@@ -22,25 +23,27 @@ for elem in sample_dict['class_b']:
     sample_dict['class_b'][elem]['marks'].update({'physics': 70})
     sample_dict['class_b'][elem]['name'] = random.choice(line)
     v.append(list(i for i in sample_dict['class_b'][elem]['marks'].values()))
-
+print(sample_dict['class_b'])
 for elem_a in sample_dict['class-_a']:
     v_a.append(list(i for i in sample_dict['class-_a'][elem_a]['marks'].values()))
-
-print(v)
+print(sample_dict['class-_a'])
+print(v, v_a)
 s = [13,44]
 f_sum = round(sum(v[0])/len(v[0]), 2)
 print(f_sum)
 s_sum = round(sum(v[1])/len(v[1]), 2)
 print(s_sum)
 sum_all_students = {'student_1': f_sum, 'student_2': s_sum}
-best_student = max(sum_all_students)
+best_student = list(sum_all_students.keys())[list(sum_all_students.values()).index(max(sum_all_students.values()))]
 print(best_student)
-v = v[0]+(v[1])
-sum_all_students_in_all_class = round(sum(v)/len(v), 2)
+new_v = v[0]+(v[1])
+sum_all_students_in_all_class = round(sum(new_v)/len(new_v), 2)
 print(sum_all_students_in_all_class)
-v_a = v_a[0]+v_a[1]
-sum_all_students_in_all_class_a = round(sum(v_a)/len(v_a), 2)
+new_v_a = v_a[0]+v_a[1]
+sum_all_students_in_all_class_a = round(sum(new_v_a)/len(new_v_a), 2)
 print(sum_all_students_in_all_class_a)
 class_sum = {'class_a': sum_all_students_in_all_class_a, 'class_b': sum_all_students_in_all_class}
-best = max(class_sum)
+value = list(class_sum.values())
+key = list(class_sum.keys())
+best = key[value.index(max(class_sum.values()))]
 print(best)
